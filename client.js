f426c2b49ae560ee267f44b5bcc3058945024794
ben hockey <neonstalwart@gmail.com>
2013-02-23 15:32:50 -0600

    allow for easier configuration of the loader

--- client.js
+++ client.js
@@ -35,39 +35,46 @@ else {
 			this.require({ packages: JSON.parse(args.packages) });
 		}
 
-		var deps = [].concat(args.suites);
-
-		if (!args.reporter) {
-			console.info('Defaulting to "console" reporter');
-			args.reporter = 'console';
-		}
-
-		// TODO: This is probably a fatal condition and so we need to let the runner know that no more information
-		// will be forthcoming from this client
-		if (typeof window !== 'undefined') {
-			window.onerror = function (message, url, lineNumber) {
-				var error = new Error(message + ' at ' + url + ':' + lineNumber);
-				topic.publish('/error', error);
-			};
-		}
-		else if (typeof process !== 'undefined') {
-			process.on('uncaughtException', function (error) {
-				topic.publish('/error', error);
-			});
-		}
-
-		// Allow 3rd party reporters to be used simply by specifying a full mid, or built-in reporters by
-		// specifying the reporter name only
-		deps.push(args.reporter.indexOf('/') > -1 ? args.reporter : './lib/reporters/' + args.reporter);
-
-		// Client interface has only one environment, the current environment, and cannot run functional tests on
-		// itself
-		main.suites.push(new Suite({ name: 'main', sessionId: args.sessionId }));
-
-		require(deps, function () {
-			if (args.autoRun !== 'false') {
-				main.run();
+		require(args.loaderConfig ? [args.loaderConfig] : [], function (loaderConfig) {
+			// configure the loader before loading any test modules
+			if (loaderConfig) {
+				this.require(loaderConfig);
 			}
+
+			var deps = [].concat(args.suites);
+
+			if (!args.reporter) {
+				console.info('Defaulting to "console" reporter');
+				args.reporter = 'console';
+			}
+
+			// TODO: This is probably a fatal condition and so we need to let the runner know that no more information
+			// will be forthcoming from this client
+			if (typeof window !== 'undefined') {
+				window.onerror = function (message, url, lineNumber) {
+					var error = new Error(message + ' at ' + url + ':' + lineNumber);
+					topic.publish('/error', error);
+				};
+			}
+			else if (typeof process !== 'undefined') {
+				process.on('uncaughtException', function (error) {
+					topic.publish('/error', error);
+				});
+			}
+
+			// Allow 3rd party reporters to be used simply by specifying a full mid, or built-in reporters by
+			// specifying the reporter name only
+			deps.push(args.reporter.indexOf('/') > -1 ? args.reporter : './lib/reporters/' + args.reporter);
+
+			// Client interface has only one environment, the current environment, and cannot run functional tests on
+			// itself
+			main.suites.push(new Suite({ name: 'main', sessionId: args.sessionId }));
+
+			require(deps, function () {
+				if (args.autoRun !== 'false') {
+					main.run();
+				}
+			});
 		});
 	});
 }
